00189c41b5ad24647a4ac1944c61721f3ea09323
Mikutut <marcinmikula840@gmail.com>
2022-09-25 01:30:58 +0200

    Add `AdHocErrors`

--- src/responses/errors.rs
+++ src/responses/errors.rs
@@ -1,5 +1,7 @@
+use std::marker::PhantomData;
 use serde::{Serialize};
 use super::*;
+use rocket::http::Status;
 
 #[derive(Debug, Serialize, Clone)]
 pub struct BulkRequestError<E: Serialize> {
@@ -12,4 +14,43 @@ pub struct BulkRequestError<E: Serialize> {
   #[serde(rename = "requestErrorData")]
   #[serde(skip_serializing_if = "Option::is_none")]
   pub request_error_data: Option<E>
+}
+
+pub struct AdHocErrors<S: Serialize, E: Serialize> {
+  _one: PhantomData<S>,
+  _two: PhantomData<E>
+}
+
+impl<S: Serialize, E: Serialize> AdHocErrors<S, E> {
+  pub fn invalid_control_key(response_builder: &mut ResponseBuilder<S, E>, control_key: &String, link_id: &String) {
+    response_builder.error(
+      Status::Unauthorized,
+      ResponseErrorType::InvalidControlKeyError,
+      format!("'{}' is not a valid control key for link with ID '{}'!",
+        control_key,
+        link_id
+      )
+    );
+  }
+  pub fn database_pool(response_builder: &mut ResponseBuilder<S, E>) {
+    response_builder.error(
+      Status::InternalServerError,
+      ResponseErrorType::DatabaseError,
+      String::from("Could not get database pool!")
+    );
+  }
+  pub fn link_id_not_found(response_builder: &mut ResponseBuilder<S, E>, link_id: &String) {
+    response_builder.error(
+      Status::NotFound,
+      ResponseErrorType::LinkNotFoundError,
+      format!("Link with ID '{}' not found!", link_id)
+    );
+  }
+  pub fn target_invalid(response_builder: &mut ResponseBuilder<S, E>, target: &String) {
+    response_builder.error(
+      Status::BadRequest,
+      ResponseErrorType::ValidationError,
+      format!("Target '{}' is not a valid URL!", target)
+    );
+  }
 }
